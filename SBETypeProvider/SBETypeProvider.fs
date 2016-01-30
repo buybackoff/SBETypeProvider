e9a534863ac73ef9b9cad53ce08f9cc156fe9a99
buybackoff <vbaybekov@gmail.com>
2016-01-30 12:33:39 +0000

    Initial commit with TP starter pack

---
+++ SBETypeProvider/SBETypeProvider.fs
@@ -0,0 +1,11 @@
+﻿namespace SBETypeProvider
+open System
+
+// proof of concept
+module SBETest =
+  let schema = "<type name=\"TheNumber\" primitiveType=\"uint16\" semanticType=\"TheNumber\"/>"
+  let payload = BitConverter.GetBytes(42us)
+
+  // need to get something like Assert.AreEqual(42us, SBE(schema, payload).TheNumber)
+
+
